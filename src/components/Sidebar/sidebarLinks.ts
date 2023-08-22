f311ccc8c51e6decf208a0d2810a85d683c29305
Onurcan Sever <onsever@icloud.com>
2023-08-22 00:01:37 -0400

    refactor

---
+++ src/components/Sidebar/sidebarLinks.ts
@@ -0,0 +1,29 @@
+type SidebarLink = {
+  title: string;
+  link: string;
+};
+
+const sidebarLinks: SidebarLink[] = [
+  {
+    title: "Terms of Service",
+    link: "/terms",
+  },
+  {
+    title: "Privacy Policy",
+    link: "/privacy",
+  },
+  {
+    title: "Cookie Policy",
+    link: "/cookie",
+  },
+  {
+    title: "Accessibility",
+    link: "/accessibility",
+  },
+  {
+    title: "Ads info",
+    link: "/ads",
+  },
+];
+
+export default sidebarLinks;
