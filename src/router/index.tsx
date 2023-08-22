cff979fb99b93699b8c991a19dcca78259a684c7
Onurcan Sever <onsever@icloud.com>
2023-08-21 23:01:21 -0400

    modify the router object

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,16 +1,27 @@
-import { createBrowserRouter, RouteObject } from "react-router-dom";
+import { createBrowserRouter, Outlet, RouteObject } from "react-router-dom";
 import HomePage from "../pages/HomePage";
 import AuthPage from "../pages/AuthPage";
 import ProtectedRoute from "./ProtectedRoute";
+import { Layout } from "../components";
 
 const routes: RouteObject[] = [
   {
     path: "/home",
     element: (
-      <ProtectedRoute>
-        <HomePage />
-      </ProtectedRoute>
+      <Layout>
+        <Outlet />
+      </Layout>
     ),
+    children: [
+      {
+        path: "/home",
+        element: (
+          <ProtectedRoute>
+            <HomePage />
+          </ProtectedRoute>
+        ),
+      },
+    ],
   },
   {
     path: "/",
