7b1c2ae1f7e6d4c24a3cc6694b07d777045f881e
Onurcan Sever <onsever@icloud.com>
2023-08-22 19:23:50 -0400

    add new nav items and refactor spacing

--- src/components/Header/navLinks.tsx
+++ src/components/Header/navLinks.tsx
@@ -1,7 +1,11 @@
 import React from "react";
-import { RiHome7Fill, RiSearchLine, RiUserLine } from "react-icons/ri";
+import { RiHome7Fill, RiSearchLine, RiFileList2Line } from "react-icons/ri";
 import { GrNotification } from "react-icons/gr";
 import { AiOutlineMail } from "react-icons/ai";
+import { FaRegUser, FaRegBookmark } from "react-icons/fa";
+import { FiUsers } from "react-icons/fi";
+import { CgMoreO } from "react-icons/cg";
+import { MdOutlineVerified } from "react-icons/md";
 
 interface NavLink {
   title: string;
@@ -31,8 +35,33 @@ const navLinks: NavLink[] = [
     active: false,
   },
   {
+    title: "Lists",
+    icon: <RiFileList2Line className="w-6 h-6" />,
+    active: false,
+  },
+  {
+    title: "Bookmarks",
+    icon: <FaRegBookmark className="w-6 h-6" />,
+    active: false,
+  },
+  {
+    title: "Communities",
+    icon: <FiUsers className="w-6 h-6" />,
+    active: false,
+  },
+  {
+    title: "Verified",
+    icon: <MdOutlineVerified className="w-6 h-6" />,
+    active: false,
+  },
+  {
     title: "Profile",
-    icon: <RiUserLine className="w-6 h-6" />,
+    icon: <FaRegUser className="w-6 h-6" />,
+    active: false,
+  },
+  {
+    title: "More",
+    icon: <CgMoreO className="w-6 h-6" />,
     active: false,
   },
 ];
