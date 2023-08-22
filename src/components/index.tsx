da54132c9d8e1bc23ecd712369ec350ee63468c7
Onurcan Sever <onsever@icloud.com>
2023-08-22 14:57:39 -0400

    refactor add new post component

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -3,3 +3,4 @@ export { default as Button } from "./Button";
 export { default as Input } from "./Input";
 export { default as Layout } from "./Layout";
 export { default as UserAvatarCircle } from "./UserAvatarCircle";
+export { default as AddNewPost } from "./AddNewPost";
