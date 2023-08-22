7f8b2ac703c9ca7dbff67783c66aa21d25c8372f
Onurcan Sever <onsever@icloud.com>
2023-08-22 11:24:34 -0400

    create slice for post and add it to the store

---
+++ src/redux/features/post/postSlice.ts
@@ -0,0 +1,55 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { UserState } from "../user/userSlice.ts";
+
+export interface PostState {
+  id: string | number;
+  content: string;
+  user: UserState | null;
+}
+
+const initialState: PostState[] = [];
+
+const postSlice = createSlice({
+  name: "post",
+  initialState,
+  reducers: {
+    createOrUpdatePost: (state, action: PayloadAction<PostState>) => {
+      const existingPostIndex = state.findIndex(
+        (post) => post.id === action.payload.id
+      );
+
+      if (existingPostIndex !== -1) {
+        // Update existing post
+        state[existingPostIndex] = action.payload;
+      } else {
+        // Create a new post
+        state.push(action.payload);
+      }
+
+      localStorage.setItem("posts", JSON.stringify(state));
+    },
+    getPosts: (state) => {
+      const postsJSON: string | null = localStorage.getItem("posts");
+
+      if (postsJSON) {
+        const posts = JSON.parse(postsJSON);
+        state.push(...posts);
+      }
+    },
+    deletePost: (state, action: PayloadAction<string | number>) => {
+      const existingPostIndex = state.findIndex(
+        (post) => post.id === action.payload
+      );
+
+      if (existingPostIndex !== -1) {
+        state.splice(existingPostIndex, 1);
+      }
+
+      localStorage.setItem("posts", JSON.stringify(state));
+    },
+  },
+});
+
+export const { createOrUpdatePost, getPosts, deletePost } = postSlice.actions;
+
+export default postSlice.reducer;
