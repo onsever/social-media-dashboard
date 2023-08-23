6c20b389bfed6a808eaa23afe46245dad267f1a9
Onurcan Sever <onsever@icloud.com>
2023-08-23 11:09:30 -0400

    refactor links for react router

--- src/components/Header/navLinks.tsx
+++ src/components/Header/navLinks.tsx
@@ -11,6 +11,7 @@ interface NavLink {
   title: string;
   icon: React.ReactNode;
   active: boolean;
+  path: string;
 }
 
 const navLinks: NavLink[] = [
@@ -18,51 +19,61 @@ const navLinks: NavLink[] = [
     title: "Home",
     icon: <RiHome7Fill className="w-7 h-7" />,
     active: true,
+    path: "/home",
   },
   {
     title: "Explore",
     icon: <RiSearchLine className="w-6 h-6" />,
     active: false,
+    path: "/explore",
   },
   {
     title: "Notifications",
     icon: <GrNotification className="w-6 h-6" />,
     active: false,
+    path: "/notifications",
   },
   {
     title: "Messages",
     icon: <AiOutlineMail className="w-6 h-6" />,
     active: false,
+    path: "/messages",
   },
   {
     title: "Lists",
     icon: <RiFileList2Line className="w-6 h-6" />,
     active: false,
+    path: "/lists",
   },
   {
     title: "Bookmarks",
     icon: <FaRegBookmark className="w-6 h-6" />,
     active: false,
+    path: "/bookmarks",
   },
   {
     title: "Communities",
     icon: <FiUsers className="w-6 h-6" />,
     active: false,
+    path: "/communities",
   },
   {
     title: "Verified",
     icon: <MdOutlineVerified className="w-6 h-6" />,
     active: false,
+    path: "/verified",
   },
   {
     title: "Profile",
     icon: <FaRegUser className="w-6 h-6" />,
     active: false,
+    path: "/profile",
   },
   {
     title: "More",
     icon: <CgMoreO className="w-6 h-6" />,
     active: false,
+    path: "/more",
   },
 ];
 
