260447544ae6aba5af58d1db4b658c82e307db13
Onurcan Sever <onsever@icloud.com>
2023-08-23 11:14:40 -0400

    change placement of the link

--- src/components/PostItem/index.tsx
+++ src/components/PostItem/index.tsx
@@ -61,96 +61,94 @@ export default function PostItem({ post, user }: PostItemProps) {
   };
 
   return (
-    <Link to={`/home/post/${post.id}`}>
-      <div className="px-4 pt-4 flex border-b border-b-gray-100 pb-4 hover:bg-gray-50 cursor-pointer">
-        <div>
-          <UserAvatarCircle user={user} />
-        </div>
-        <div className="w-full">
-          <div className="ml-4">
-            <div className="flex justify-between">
-              <div className="flex items-center">
+    <div className="px-4 pt-4 flex border-b border-b-gray-100 pb-4 hover:bg-gray-50 cursor-pointer">
+      <div>
+        <UserAvatarCircle user={user} />
+      </div>
+      <div className="w-full">
+        <div className="ml-4">
+          <div className="flex justify-between">
+            <div className="flex items-center">
+              <Link to={`/home/post/${post.id}`}>
                 <span className="font-bold">{user.fullName}</span>
-                <span className="text-gray-500 ml-1">@{user.username}</span>
-                <span className="text-gray-500 ml-1">·</span>
-                <span className="text-gray-500 ml-1">1h</span>
-              </div>
-              <div
-                className="flex items-center relative"
-                onClick={() => setIsMoreIconClicked((prevState) => !prevState)}
-              >
-                <RiMoreLine className="text-gray-500 text-xl cursor-pointer" />
-                {isMoreIconClicked && (
-                  <div className="absolute right-0 top-5 bg-white shadow-lg rounded-lg w-32">
-                    <ul className="flex flex-col">
-                      <li
-                        className="px-4 py-2 hover:bg-gray-200 cursor-pointer"
-                        onClick={() => setIsEditMode((prevState) => !prevState)}
-                      >
-                        Edit
-                      </li>
-                      <li
-                        className="px-4 py-2 hover:bg-gray-200 cursor-pointer"
-                        onClick={() => handleDeletePost(post.id)}
-                      >
-                        Delete
-                      </li>
-                    </ul>
-                  </div>
-                )}
-              </div>
+              </Link>
+              <span className="text-gray-500 ml-1">@{user.username}</span>
+              <span className="text-gray-500 ml-1">·</span>
+              <span className="text-gray-500 ml-1">1h</span>
             </div>
-            <div className="mt-1">
-              {isEditMode ? (
-                <form>
-                  <textarea
-                    className="w-full h-32 p-2 rounded-lg border-none focus:outline-none resize-none"
-                    value={post.content}
-                    onChange={(e) => handleEditPost(e, post.id)}
-                    onKeyDown={(e) => {
-                      if (e.key === "Enter" && !e.shiftKey) {
-                        handleSubmit(e);
-                      }
-                    }}
-                  />
-                </form>
-              ) : (
-                <p>{post.content}</p>
+            <div
+              className="flex items-center relative"
+              onClick={() => setIsMoreIconClicked((prevState) => !prevState)}
+            >
+              <RiMoreLine className="text-gray-500 text-xl cursor-pointer" />
+              {isMoreIconClicked && (
+                <div className="absolute right-0 top-5 bg-white shadow-lg rounded-lg w-32">
+                  <ul className="flex flex-col">
+                    <li
+                      className="px-4 py-2 hover:bg-gray-200 cursor-pointer"
+                      onClick={() => setIsEditMode((prevState) => !prevState)}
+                    >
+                      Edit
+                    </li>
+                    <li
+                      className="px-4 py-2 hover:bg-gray-200 cursor-pointer"
+                      onClick={() => handleDeletePost(post.id)}
+                    >
+                      Delete
+                    </li>
+                  </ul>
+                </div>
               )}
             </div>
           </div>
-          {/* Icons Container */}
-          <div className="ml-2 mt-3 flex items-center justify-between">
-            <div className="flex items-center">
-              <div className="flex items-center hover:bg-primaryLight cursor-pointer p-2 rounded-full">
-                <RiChat3Line className="text-gray-500 text-xl cursor-pointer" />
-                <span className="text-gray-500 ml-2 text-sm">
-                  {post.comments}
-                </span>
-              </div>
-              <div className="flex items-center ml-8 hover:bg-green-100 cursor-pointer p-2 rounded-full">
-                <LiaRedoAltSolid className="text-gray-500 text-xl cursor-pointer" />
-                <span className="text-gray-500 ml-2 text-sm">
-                  {post.shares}
-                </span>
-              </div>
-              <div className="flex items-center ml-8 hover:bg-red-100 cursor-pointer p-2 rounded-full">
-                <AiOutlineHeart className="text-gray-500 text-xl cursor-pointer" />
-                <span className="text-gray-500 ml-2 text-sm">{post.likes}</span>
-              </div>
-              <div className="flex items-center ml-8 hover:bg-primaryLight cursor-pointer p-2 rounded-full">
-                <IoMdStats className="text-gray-500 text-xl cursor-pointer" />
-                <span className="text-gray-500 ml-2 text-sm">
-                  {formatNumberToK(post.stats)}
-                </span>
-              </div>
+          <div className="mt-1">
+            {isEditMode ? (
+              <form>
+                <textarea
+                  className="w-full h-32 p-2 rounded-lg border-none focus:outline-none resize-none"
+                  value={post.content}
+                  onChange={(e) => handleEditPost(e, post.id)}
+                  onKeyDown={(e) => {
+                    if (e.key === "Enter" && !e.shiftKey) {
+                      handleSubmit(e);
+                    }
+                  }}
+                />
+              </form>
+            ) : (
+              <p>{post.content}</p>
+            )}
+          </div>
+        </div>
+        {/* Icons Container */}
+        <div className="ml-2 mt-3 flex items-center justify-between">
+          <div className="flex items-center">
+            <div className="flex items-center hover:bg-primaryLight cursor-pointer p-2 rounded-full">
+              <RiChat3Line className="text-gray-500 text-xl cursor-pointer" />
+              <span className="text-gray-500 ml-2 text-sm">
+                {post.comments}
+              </span>
+            </div>
+            <div className="flex items-center ml-8 hover:bg-green-100 cursor-pointer p-2 rounded-full">
+              <LiaRedoAltSolid className="text-gray-500 text-xl cursor-pointer" />
+              <span className="text-gray-500 ml-2 text-sm">{post.shares}</span>
+            </div>
+            <div className="flex items-center ml-8 hover:bg-red-100 cursor-pointer p-2 rounded-full">
+              <AiOutlineHeart className="text-gray-500 text-xl cursor-pointer" />
+              <span className="text-gray-500 ml-2 text-sm">{post.likes}</span>
             </div>
             <div className="flex items-center ml-8 hover:bg-primaryLight cursor-pointer p-2 rounded-full">
-              <MdIosShare className="text-gray-500 text-xl cursor-pointer" />
+              <IoMdStats className="text-gray-500 text-xl cursor-pointer" />
+              <span className="text-gray-500 ml-2 text-sm">
+                {formatNumberToK(post.stats)}
+              </span>
             </div>
+          </div>
+          <div className="flex items-center ml-8 hover:bg-primaryLight cursor-pointer p-2 rounded-full">
+            <MdIosShare className="text-gray-500 text-xl cursor-pointer" />
           </div>
         </div>
       </div>
-    </Link>
+    </div>
   );
 }
